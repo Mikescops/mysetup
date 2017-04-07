e579ff1dfb00e3635fd988e7c8167f4a5bf06ce3
Corentin Mors <mikescops@pixelswap.fr>
2017-04-07 21:20:52 +0200

    First preview of homepage

---
+++ static_html/js/app.js
@@ -0,0 +1,29 @@
+$('.home_slider').slick({
+  centerMode: true,
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  centerPadding: '200px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
