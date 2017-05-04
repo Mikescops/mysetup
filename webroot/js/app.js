55c36a7030d102b86a111406d2e98af35f2695ac
root <mikescops@pixelswap.fr>
2017-05-04 19:45:07 +0200

    Rework old multipe image upload js/css + js fixes

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -88,7 +88,7 @@ $('.post_slider').slick({
         .addClass( SHOW_CLASS )
         .hide()
         .fadeIn( 550 );
-  });
+  });});
 
 /***** On load functions *****/
 $(function(){
@@ -104,11 +104,11 @@ $(function(){
    });
     $("#social-networks").jsSocials({
       shareIn: "popup",
-      showCount: true,
+      showCount: false,
           showLabel: function(screenWidth) {
         return (screenWidth > 640);
       },
-      shares: ["twitter", "facebook", "googleplus", "pinterest", "stumbleupon", "whatsapp"]
+      shares: ["twitter", "facebook", "googleplus", "pinterest", "whatsapp"]
     });
 
     $("#profileImage").click(function(e) {
@@ -156,6 +156,39 @@ $(function(){
     $("#gallery4").change(function(){
       galleryPreview_edit( this, 4 );
     });
+
+    /* Same for add */
+    $("#gallery0image_preview_add").click(function(e) {
+      $("#gallery0add").click();
+    });
+    $("#gallery1image_preview_add").click(function(e) {
+      $("#gallery1add").click();
+    });
+    $("#gallery2image_preview_add").click(function(e) {
+      $("#gallery2add").click();
+    });
+    $("#gallery3image_preview_add").click(function(e) {
+      $("#gallery3add").click();
+    });
+    $("#gallery4image_preview_add").click(function(e) {
+      $("#gallery4add").click();
+    });
+
+    $("#gallery0add").change(function(){
+      galleryPreview_add( this, 0 );
+    });
+    $("#gallery1add").change(function(){
+      galleryPreview_add( this, 1 );
+    });
+    $("#gallery2add").change(function(){
+      galleryPreview_add( this, 2 );
+    });
+    $("#gallery3add").change(function(){
+      galleryPreview_add( this, 3 );
+    });
+    $("#gallery4add").change(function(){
+      galleryPreview_add( this, 4 );
+    });
 });
 
 /********* Update Profile Picture on Upload *********/
@@ -190,75 +223,12 @@ function galleryPreview_edit( uploader, number ) {
     }
 }
 
-  /***************** DRAG AND DROP ****************/
-
-  // getElementById
-  function $id(id) {
-    return document.getElementById(id);
-  }
-
-  // output information
-  function Output(msg) {
-    var m = $id("messages");
-    m.innerHTML = msg + m.innerHTML;
-  }
-
-  // file drag hover
-  function FileDragHover(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    e.target.className = (e.type == "dragover" ? "hover" : "");
-  }
-
-  // file selection
-  function FileSelectHandler(e) {
-    // cancel event and hover styling
-    FileDragHover(e);
-    // fetch FileList object
-    var files = e.target.files || e.dataTransfer.files;
-
-    // process all File objects
-    for (var i = 0, f; f = files[i]; i++) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $('<img />').attr('src',  e.target.result).appendTo(images_holder);
-        }
-        reader.readAsDataURL(files[i]);
-      //ParseFile(f);
+function galleryPreview_add( uploader, number ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#gallery'+number+'image_preview_add').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
     }
-  }
-
-  // output file information
-  function ParseFile(file) {
-    Output(
-      "<p>File information: <strong>" + file.name +
-      "</strong> type: <strong>" + file.type +
-      "</strong> size: <strong>" + file.size +
-      "</strong> bytes</p>"
-    );
-  }
-
-  // initialize
-  function Init() {
-    var fileselect = $id("fileselect");
-        //var filedrag = $id("filedrag");
-    // file select
-    fileselect.addEventListener("change", FileSelectHandler, false);
-    // is XHR2 available?
-    var xhr = new XMLHttpRequest();
-    // if (xhr.upload) {
-    //   // file drop
-    //   filedrag.addEventListener("dragover", FileDragHover, false);
-    //   filedrag.addEventListener("dragleave", FileDragHover, false);
-    //   filedrag.addEventListener("drop", FileSelectHandler, false);
-    //   filedrag.style.display = "block";
-    // }
-  }
-  // call initialization file
-  if (window.File && window.FileList && window.FileReader) {
-    Init();
-  }
-})();
+}
 
 
 /* AMAZON ADD ITEM */
