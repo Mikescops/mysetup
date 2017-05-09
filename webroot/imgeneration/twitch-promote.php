6084609cea24ad78d789480c7add9c77600084db
root <mikescops@pixelswap.fr>
2017-05-09 15:16:34 +0200

    Finish image generation with all parameters

--- webroot/imgeneration/twitch-promote.php
+++ webroot/imgeneration/twitch-promote.php
@@ -3,12 +3,13 @@
 
 $name = "Partagé par " . $_GET['name'];
 $setup = $_GET['setup'];
+$id = $_GET['id'];
 
 Header ("Content-type: image/jpeg");
 $image = imageCreateFromJPEG("partner_banner.jpg");
 
-$pfile = '../img/profile-default.png';
-$profile = imagecreatefrompng($pfile);
+$pfile = '../uploads/files/pics/profile_picture_'.$id.'.png';
+$profile = imagecreatefromPNG($pfile);
 list($pwidth, $pheight) = getimagesize($pfile);
 
 $color = ImageColorAllocate($image, 255, 255, 255);
@@ -16,11 +17,19 @@ $color = ImageColorAllocate($image, 255, 255, 255);
 imagealphablending($image, true);
 imagesavealpha($image, true);
 
-imagecopyresampled($image, $profile, 8, 245, 0, 0, 70, 70, $pwidth, $pheight);
+imagecopyresampled($image, $profile, 0, 239, 0, 0, 80, 80, $pwidth, $pheight);
 
 // Write names.
-imagettftext($image, 11, 0, 90, 305, $color, 'corbel.ttf', $name);
-imagettftext($image, 17, 0, 90, 275, $color, 'corbel.ttf', $setup);
+if(strlen($setup) > 20){
+	$setup = substr($setup, 0, 40);
+	$setup1 = wordwrap($setup, 20, "\n");	
+	imagettftext($image, 15, 0, 88, 260, $color, 'corbel.ttf', $setup1);
+	imagettftext($image, 11, 0, 88, 308, $color, 'corbel.ttf', $name);
+}
+else{
+	imagettftext($image, 17, 0, 88, 277, $color, 'corbel.ttf', $setup);
+	imagettftext($image, 11, 0, 88, 300, $color, 'corbel.ttf', $name);
+}
 
 // Return output.
 ImageJPEG($image, NULL, 93);
