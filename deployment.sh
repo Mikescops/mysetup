ff28f7255096297b2f499c903e00b83d99d909c7
Samuel FORESTIER <sam.forestier@gmail.com>
2017-08-02 17:06:08 +0200

    We will clean the cache BEFORE run `composer install`

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 
+# This OUR method to clean the cache :P
+rm -rf tmp/*
+
 # Let's upgrade the vendor packages with the latest versions (should be tested during development)
 composer install
 
@@ -10,9 +13,6 @@ git checkout vendor/loadsys/cakephp_sitemap/
 # Some CakePHP optimizations
 composer dumpautoload -o
 bin/cake plugin assets symlink
-
-# This OUR method to clean the cache :P
-rm -rf tmp/*
 
 
 exit 0
