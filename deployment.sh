c667bf3231cb1a76079a350058de783a9d921211
Samuel FORESTIER <sam.forestier@gmail.com>
2017-09-15 11:44:57 -0400

    Now `composer` should not be asking for a "Yes"

--- deployment.sh
+++ deployment.sh
@@ -5,7 +5,7 @@
 rm -rf tmp/*
 
 # Let's upgrade the vendor packages with the latest versions (should be tested during development)
-composer install
+composer install -n
 
 # We can't get Composer to do what we want with this tweaked dependence, so this is the current workaround...
 git checkout vendor/loadsys/cakephp_sitemap/
