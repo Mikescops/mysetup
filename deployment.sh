d3956ca79a5aecd6bfe41ca5f14a8523b6dfa3d1
Samuel FORESTIER <sam.forestier@gmail.com>
2017-10-01 19:23:06 -0400

    Gets rid of CakePHP_Sitemap sources here and use a tweaked third repo'

--- deployment.sh
+++ deployment.sh
@@ -7,9 +7,6 @@ rm -rf tmp/*
 # Let's upgrade the vendor packages with the latest versions (should be tested during development)
 composer install -n
 
-# We can't get Composer to do what we want with this tweaked dependence, so this is the current workaround...
-git checkout vendor/loadsys/cakephp_sitemap/
-
 # Some CakePHP optimizations
 composer dumpautoload -o
 bin/cake plugin assets symlink
