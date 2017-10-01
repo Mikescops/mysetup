2b43ed8942916221f53c0accad68f162be6903aa
Samuel FORESTIER <sam.forestier@gmail.com>
2017-09-30 21:53:23 -0400

    Prepares API's URL for deployment

--- webroot/api/widgets.js
+++ webroot/api/widgets.js
@@ -1,3 +1,3 @@
 function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
 r(function(){var frame=document.getElementById('mysetup-embed');var iframe=document.createElement('iframe');iframe.frameBorder=0;iframe.scrolling="no";iframe.setAttribute("onload","resizeIframe(this)");iframe.setAttribute("style","display:block;")
-iframe.height=0;iframe.width=frame.getAttribute('ms-width');iframe.src='http://localhost/mysetup/embed/'+frame.getAttribute('ms-owner');frame.innerHTML="";frame.prepend(iframe)});function resizeIframe(obj){obj.style.height=obj.contentWindow.document.body.scrollHeight+'px'}
+iframe.height=0;iframe.width=frame.getAttribute('ms-width');iframe.src='https://mysetup.co/embed/'+frame.getAttribute('ms-owner');frame.innerHTML="";frame.prepend(iframe)});function resizeIframe(obj){obj.style.height=obj.contentWindow.document.body.scrollHeight+'px'}
