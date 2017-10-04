85f657f174d700cbf20ee0922990e2aabdeb1574
Corentin <corentin.mors@gmail.com>
2017-10-04 17:19:42 +0200

    Optimize loading of resize and set default value of height

--- webroot/api/widgets.js
+++ webroot/api/widgets.js
@@ -1,3 +1,3 @@
 function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
-r(function(){var frame=document.getElementById('mysetup-embed');var iframe=document.createElement('iframe');iframe.frameBorder=0;iframe.scrolling="no";iframe.setAttribute("onload","resizeIframe(this)");iframe.setAttribute("style","display:block;")
-iframe.height=0;iframe.width=frame.getAttribute('ms-width');iframe.src='https://mysetup.co/embed/'+frame.getAttribute('ms-setup');frame.innerHTML="";frame.prepend(iframe)});function resizeIframe(obj){obj.height=(obj.width*75)/100+'px'}
+r(function(){var frame=document.getElementById('mysetup-embed');var iframe=document.createElement('iframe');iframe.frameBorder=0;iframe.scrolling="no";iframe.setAttribute("style","display:block;")
+iframe.height=0;if(frame.getAttribute('ms-width')){iframe.width=frame.getAttribute('ms-width')}else{iframe.width=400}iframe.height=(iframe.width*75)/100;iframe.src='https://mysetup.co/embed/'+frame.getAttribute('ms-setup');frame.innerHTML="";frame.prepend(iframe)})
