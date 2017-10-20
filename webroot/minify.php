3cb2a8635cb10428f33a336c25d0de5dcac03bd3
Corentin <corentin.mors@gmail.com>
2017-10-20 15:12:49 +0200

    Rework minifier > add js support and optimize output + active mod_deflate

--- webroot/minify.php
+++ webroot/minify.php
@@ -1,3 +1,4 @@
+<header><title>msMinifier</title></header>
 <?php
 
 require dirname(__DIR__) . '/vendor/autoload.php';
@@ -7,6 +8,9 @@ session_start();
 
 if($_SESSION['Auth']['User']['admin'])
 {
+
+    echo '<hr>RENDERED OUTPUT OF MINIFIER<hr>';
+
     $base = dirname(__DIR__) . '/webroot/css/';
 
     $minifier = (new \MatthiasMullie\Minify\CSS())
@@ -19,9 +23,18 @@ if($_SESSION['Auth']['User']['admin'])
                     ->add($base . 'jssocials-theme-flat.css')
                     ->add($base . 'style.css');
 
-    echo $minifier->minify($base . 'app.min.css');
-}
+    dump($minifier->minify($base . 'app.min.css'));
 
+    echo '<hr>CSS IS MINIFIED AND SAVED<hr>';
+
+    $base = dirname(__DIR__) . '/webroot/js/';
+
+    $minifierjs = (new \MatthiasMullie\Minify\JS())->add($base . 'app.js');
+
+    dump($minifierjs->minify($base . 'app.min.js')); //Dump is used to prevent javascript activation in minifier
+
+    echo '<hr>JS IS UGLYFIED AND SAVED<hr>';
+}
 else
 {
     echo "You don't have the rights to access this service.";
