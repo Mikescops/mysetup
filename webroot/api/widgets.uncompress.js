9349c80953b5e443edb1fc3373a233110f53291a
Samuel FORESTIER <dev@samuel.domains>
2018-03-17 11:43:41 +0100

    Make `widgets.js` comply with `jshint`

--- webroot/api/widgets.uncompress.js
+++ webroot/api/widgets.uncompress.js
@@ -1,6 +1,6 @@
-/**** EMBED API mySetup.co v2.0.1 ****/
+/**** EMBED API mySetup.co v3.1.0 ****/
 function r(f) {
-    /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f()
+    /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f();
 }
 r(function() {
     var frame = document.getElementById('mysetup-embed');
@@ -14,8 +14,8 @@ r(function() {
     } else {
         iframe.width = '100%';
         iframe.height = '100%';
-        iframe.setAttribute("style", "display:block;position:absolute;top:0;left:0"); 
-        frame.setAttribute("style", "position:relative;padding-bottom: 75%"); 
+        iframe.setAttribute("style", "display:block;position:absolute;top:0;left:0");
+        frame.setAttribute("style", "position:relative;padding-bottom: 75%");
     }
     if (frame.getAttribute('dev') == 'on') {
         iframe.src = document.location.origin + '/api/embed/' + frame.getAttribute('ms-setup');
@@ -23,5 +23,5 @@ r(function() {
         iframe.src = 'https://mysetup.co/api/embed/' + frame.getAttribute('ms-setup');
     }
     frame.innerHTML = "";
-    frame.prepend(iframe)
-})
+    frame.prepend(iframe);
+});
