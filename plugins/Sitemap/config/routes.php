94e309fb08dccf8e26c7061d7b296b7ee01dcad5
Samuel FORESTIER <dev@samuel.domains>
2018-06-21 18:08:46 +0200

    Removes the additional route check previously added in (now useless)

--- plugins/Sitemap/config/routes.php
+++ plugins/Sitemap/config/routes.php
@@ -2,15 +2,13 @@
 
 use Cake\Routing\Router;
 
-if(!Router::routeExists(['_name' => 'sitemap'])) {
-	Router::plugin(
-		'Sitemap',
-		['path' => '/sitemap'],
-		function ($routes) {
-			$routes->connect('/*', [
-				'controller' => 'Sitemaps',
-				'action' => 'index'
-			], ['_name' => 'sitemap']);
-		}
-	);
-}
+Router::plugin(
+    'Sitemap',
+    ['path' => '/sitemap'],
+    function ($routes) {
+        $routes->connect('/*', [
+            'controller' => 'Sitemaps',
+            'action' => 'index'
+        ], ['_name' => 'sitemap']);
+    }
+);
