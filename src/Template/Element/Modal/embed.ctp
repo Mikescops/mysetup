45f09e8b2d5659a458e02f26b99a0a0fc631f265
Corentin <mikescops@pixelswap.fr>
2019-01-11 15:24:29 +0100

    Let users embed multiple setups on a same page (need modify samuel's gist)

--- src/Template/Element/Modal/embed.ctp
+++ src/Template/Element/Modal/embed.ctp
@@ -2,14 +2,14 @@
     <h4><?= __('How to embed the setup on my website ?') ?></h4>
     <?= __("It's pretty easy, just add the code below to your page (and set the setup id accordingly)") ?> :
 
-    <div class="input text"><input readonly name="embedcode" id="embedcode" value='<script src="<?= $this->Url->build('/api/widgets.js', true) ?>"></script><div id="mysetup-embed" ms-setup="<?= $setup->id ?>" ms-width="responsive"><?= __('Setup shared by') ?> <?= $this->Html->link($setup->user['name'], ['controller' => 'users', 'action' => 'view', $setup->user['id'], '_full' => true]) ?> <?= __x('"sur" un site', 'on') ?> <a href="<?= $this->Url->build('/', true) ?>">mySetup.co</a></div>' type="text"></div>
+    <div class="input text"><input readonly name="embedcode" id="embedcode" value='<script src="<?= $this->Url->build('/api/widgets.js', true) ?>"></script><div class="mysetup-embed" ms-setup="<?= $setup->id ?>" ms-width="responsive"><?= __('Setup shared by') ?> <?= $this->Html->link($setup->user['name'], ['controller' => 'users', 'action' => 'view', $setup->user['id'], '_full' => true]) ?> <?= __x('"sur" un site', 'on') ?> <a href="<?= $this->Url->build('/', true) ?>">mySetup.co</a></div>' type="text"></div>
 
     <h5><?= __('Preview') ?> :</h5>
 
     <div class="display-preview">
 
         <script async src="<?= $this->Url->build('/api/widgets.js') ?>"></script>
-        <div id="mysetup-embed" <?= ($debug ? 'dev="on"' : '') ?> ms-setup="<?= $setup->id ?>" ms-width="responsive"><?= __('Setup shared by') ?> <?= $this->Html->link($setup->user['name'], ['controller' => 'users', 'action' => 'view', $setup->user['id'], '_full' => true]) ?> <?= __x('"sur" un site', 'on') ?> <a href="<?= $this->Url->build('/', true) ?>">mySetup.co</a></div>
+        <div class="mysetup-embed" <?= ($debug ? 'dev="on"' : '') ?> ms-setup="<?= $setup->id ?>" ms-width="responsive"><?= __('Setup shared by') ?> <?= $this->Html->link($setup->user['name'], ['controller' => 'users', 'action' => 'view', $setup->user['id'], '_full' => true]) ?> <?= __x('"sur" un site', 'on') ?> <a href="<?= $this->Url->build('/', true) ?>">mySetup.co</a></div>
 
     </div>
     <br>
--- webroot/api/widgets.uncompress.js
+++ webroot/api/widgets.uncompress.js
@@ -1,27 +1,33 @@
-/**** EMBED API mySetup.co v3.1.0 ****/
+/** @cc_on EMBED API mySetup.co v3.2.0 */
+
 function r(f) {
     /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f();
 }
 r(function() {
-    var frame = document.getElementById('mysetup-embed');
-    var iframe = document.createElement('iframe');
-    iframe.frameBorder = 0;
-    iframe.scrolling = "no";
-    iframe.height = 0;
-    if (frame.getAttribute('ms-width') && frame.getAttribute('ms-width') != 'responsive') {
-        iframe.width = frame.getAttribute('ms-width');
-        iframe.height = (iframe.width * 75) / 100;
-    } else {
-        iframe.width = '100%';
-        iframe.height = '100%';
-        iframe.setAttribute("style", "display:block;position:absolute;top:0;left:0");
-        frame.setAttribute("style", "position:relative;padding-bottom: 75%");
+    var frames = document.getElementsByClassName('mysetup-embed');
+
+    for (i = 0; i < frames.length; i++) {
+        let item = frames[i];
+        let iframe = document.createElement('iframe');
+        iframe.frameBorder = 0;
+        iframe.scrolling = "no";
+        iframe.height = 0;
+        if (item.getAttribute('ms-width') && item.getAttribute('ms-width') != 'responsive') {
+            iframe.width = item.getAttribute('ms-width');
+            iframe.height = (iframe.width * 75) / 100;
+        } else {
+            iframe.width = '100%';
+            iframe.height = '100%';
+            iframe.setAttribute("style", "display:block;position:absolute;top:0;left:0");
+            item.setAttribute("style", "position:relative;padding-bottom: 75%");
+        }
+        if (item.getAttribute('dev') == 'on') {
+            iframe.src = document.location.origin + '/api/embed/' + item.getAttribute('ms-setup');
+        } else {
+            iframe.src = 'https://mysetup.co/api/embed/' + item.getAttribute('ms-setup');
+        }
+        item.innerHTML = "";
+        item.prepend(iframe);
     }
-    if (frame.getAttribute('dev') == 'on') {
-        iframe.src = document.location.origin + '/api/embed/' + frame.getAttribute('ms-setup');
-    } else {
-        iframe.src = 'https://mysetup.co/api/embed/' + frame.getAttribute('ms-setup');
-    }
-    frame.innerHTML = "";
-    frame.prepend(iframe);
+    
 });
