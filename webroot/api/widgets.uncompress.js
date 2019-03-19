4291deee3133dcc37a688d87c66f39692cc1748f
Corentin <mikescops@pixelswap.fr>
2019-03-19 19:54:25 +0100

    Lint widgets assets

--- webroot/api/widgets.uncompress.js
+++ webroot/api/widgets.uncompress.js
@@ -3,26 +3,26 @@
 function r(f) {
     /in/.test(document.readyState) ? setTimeout('r(' + f + ')', 9) : f();
 }
-r(function() {
+r(function () {
     var frames = document.getElementsByClassName('mysetup-embed');
 
-    if(frames.length > 0){
+    if (frames.length > 0) {
         for (i = 0; i < frames.length; i++) {
             renderFrame(frames[i]);
         }
     }
-    else{
+    else {
         frames = document.getElementById('mysetup-embed');
-        if(frames == null){
+        if (frames == null) {
             console.warn("[MYSETUP EMBED API] No mysetup.co frame found, please use our documentation to handle errors.");
         }
-        else{
+        else {
             console.info("[MYSETUP EMBED API] Fallback to 'id' attribute, please consider using 'class' instead.");
             renderFrame(frames);
         }
     }
 
-    function renderFrame(item){
+    function renderFrame(item) {
         let iframe = document.createElement('iframe');
         iframe.frameBorder = 0;
         iframe.scrolling = "no";
